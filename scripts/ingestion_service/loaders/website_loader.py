f004eaa091d2be75f092121632f68afcf4b762ee
S <com.com>
2025-10-06 00:45:18 +0300

    website edit

--- scripts/ingestion_service/loaders/website_loader.py
+++ scripts/ingestion_service/loaders/website_loader.py
@@ -7,7 +7,10 @@ async def crawl_website(start_url: str, **kwargs) -> str:
     # Для полноценного рекурсивного обхода ее нужно будет усложнить.
     async with aiohttp.ClientSession() as session:
         try:
-            async with session.get(start_url, ssl=False, timeout=30) as response:
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+            }
+            async with session.get(start_url, ssl=False, timeout=30, headers=headers) as response:
                 response.raise_for_status()
                 html = await response.text()
                 soup = BeautifulSoup(html, 'html.parser')
